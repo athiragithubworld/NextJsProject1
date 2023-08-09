6828a53d7450f9d669c2d54e7a51d6fe2e3ee3c5
athiragithubworld <athirahrms@gmail.com>
2023-08-09 11:45:34 +0530

    aboutusIndex.js and developer js page (Task - 2) Nested Routes

---
+++ pages/aboutus/aboutusIndex.js
@@ -0,0 +1,11 @@
+// our domain.com/aboutus/aboutusIndex
+
+function ABOUTPAGE() {
+  return (
+    <div>
+      <h1> About us Page </h1>
+    </div>
+  );
+}
+
+export default ABOUTPAGE;
---
+++ pages/aboutus/developer.js
@@ -0,0 +1,11 @@
+// our domain.com/aboutus/developer
+
+function DeveloperPage() {
+  return (
+    <div>
+      <h1> Athira FrontEnd Developer </h1>
+    </div>
+  );
+}
+
+export default DeveloperPage;
