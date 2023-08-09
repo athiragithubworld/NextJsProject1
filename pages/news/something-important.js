a5aa2574530d773d5fa8b431c49011fe8f75ac0b
athiragithubworld <athirahrms@gmail.com>
2023-08-09 11:44:06 +0530

    newsIndex.js and soemthing-imp (Task -2 ) Nested Routes

---
+++ pages/news/something-important.js
@@ -0,0 +1,11 @@
+// our domain.com/news/something-important
+
+function SomethingImportant() {
+  return (
+    <div>
+      <h1> News Sub Page </h1>
+    </div>
+  );
+}
+
+export default SomethingImportant;
