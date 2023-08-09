ca6b754c30bc17799d1ecc16d4b56048cdb6703c
athiragithubworld <athirahrms@gmail.com>
2023-08-09 19:41:44 +0530

    aboutus .js page(Task -3) show developers name on click  it  will show developer name and position using Link.

---
+++ pages/aboutus/aboutus.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+
+const aboutus = () => {
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+
+  return (
+    <div>
+      {details.map((item) => {
+        return (
+          <ul>
+            <li>
+              <Link href={`/aboutus/${item.id}`}>{item.name}</Link>
+            </li>
+          </ul>
+        );
+      })}
+    </div>
+  );
+};
+
+export default aboutus;
